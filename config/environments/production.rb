fcbb640c063cdaa87668e2e5265fd641af14120a
Louis Adjanohoun <marcellinadjanohoun@yahoo.fr>
2022-02-27 08:58:37 +0100

    - boilerplate renamed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "ikliin_production"
+  # config.active_job.queue_name_prefix = "rab_app_production"
 
   config.action_mailer.perform_caching = false
 
